f4a46068b55d879c54e07957ebf0496035906d65
Lenrow <leoeberle@gmx.de>
2023-01-02 18:47:31 +0100

    create rudimentary Main page, modernize color scheme, change switchpage function to make it more event-driven

--- JS-scripts/Firstpage.js
+++ JS-scripts/Firstpage.js
@@ -6,6 +6,7 @@ const nameForm = document.getElementById("startForm");
 const joinNewButton = document.getElementById("sendAndJoinButton");
 const remindingFormText = document.getElementById("hiddenFormText");
 
+/*
 function switchPage() {
     if (main.style.display != "none") {
         main.style.display = "none";
@@ -16,12 +17,18 @@ function switchPage() {
         main.style.display = "block";
     }
 }
+*/
+
+document.querySelectorAll(".switch-button").forEach((button) => {
+  button.addEventListener("click", function () {
+    document.querySelector(".container").classList.toggle("active");
+  });
+});
 
 function joinAnew() {
-    if (nameForm.elements[0].value && nameForm.elements[1].value){
-        location.href = "zahtmlfiles/mainPage.html";
-    }
-    else {
-        remindingFormText.style.visibility = "visible";
-    }
+  if (nameForm.elements[0].value && nameForm.elements[1].value) {
+    location.href = "zahtmlfiles/mainPage.html";
+  } else {
+    remindingFormText.style.visibility = "visible";
+  }
 }
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Webproject-Test
+
+Project-Roadmap:
+
+1. Create Texteditor to write Notes
+2. Create System to story texteditor information to then save them in a json file
+3. Implement Algorithm to display notes in the main json file as multiple Notes on the Mainpage
+4. Make it possible to download the json file or to upload another json file at the start
+5. Create Searchfunction
+6. Implement switching between Notebooks
+7. Develope a feature that makes the creation of custom tags and the applying of these to notes possible
+8. Implement sorting and filtering via tags
+9. implement uploading of pictures
