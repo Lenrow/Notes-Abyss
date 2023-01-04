a11db39bf41e8ef1eeca1ff90b27443399d1a48c
Lenrow <leoeberle@gmx.de>
2023-01-04 13:25:33 +0100

    Add function to create or check for NoteBooks obj.

--- JS-scripts/mainPage.js
+++ JS-scripts/mainPage.js
@@ -1,4 +1,17 @@
-const urlParams = new URLSearchParams(window.location.search);
-const params = Object.fromEntries(urlParams.entries());
+let NoteBooks;
+
+window.onload = function () {
+  checkForNoteBooks();
+};
+
+function checkForNoteBooks() {
+  if (NoteBooks === undefined) {
+    const params = Object.fromEntries(
+      new URLSearchParams(window.location.search).entries()
+    );
+    NoteBooks = params;
+    NoteBooks.notebook = [{ nameNoteBook: params.notebook, notes: {} }];
+  }
+}
 
 const NoteBooks = JSON.stringify(params);
