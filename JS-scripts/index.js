d5d98efb53a3c7a62bc6a3903a1ce150dceaaca2
Lenrow <leoeberle@gmx.de>
2023-01-07 16:53:37 +0100

    Create upload Method for now following page Merge mainPage.html and index.html will be merged into index.html to make uploading possible

--- JS-scripts/index.js
+++ JS-scripts/index.js
@@ -1,5 +1,23 @@
+const hiddenKeyButton = document.getElementById("hiddenButton");
+const fileInput = document.getElementById("acceptKey");
+
 for (const element of document.getElementsByClassName("switch-button")) {
   element.addEventListener("click", function () {
     document.querySelector(".container").classList.toggle("active");
   });
 }
+
+function submitJSONFile() {
+  if (fileInput.files.length > 0) {
+    fileInput.files[0].text().then(function (text) {
+      const fileObj = JSON.parse(text);
+      sessionStorage.setItem("NoteBooks", JSON.stringify(fileObj));
+    });
+  }
+}
+
+fileInput.addEventListener("change", function () {
+  hiddenKeyButton.classList.toggle("active");
+});
+
+hiddenKeyButton.addEventListener("click", submitJSONFile);
